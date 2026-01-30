a78a54a669f2a10bf29d23857a227a04551de5b8
pluuto960 <189347604+pluuto960@users.noreply.github.com>
2026-01-30 17:59:12 +0200

    ulesanne3

--- NodeJS/ulesanne3/server.js
+++ NodeJS/ulesanne3/server.js
@@ -1,14 +1,49 @@
-const http = require('http');
-constfs=require('fs');
+const {readFile} = require('fs');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  console.log(req.method, req.url);
-  res.write('<h1>Tervitused serverist</h1>');
-  res.write('<p>See tekst tuli Node.js serverist</p>');
-  res.end();
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    readFile('./assets/loomad.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        const loomad = JSON.parse(data);
+
+        res.render('index', { title: 'Avaleht', loomad: loomad });
+    });
 });
 
-server.listen(3000, () => {
-    console.log('Server töötab pordil 3000');
+app.get('/meist', (req, res) => {
+    res.render('meist', { title: 'Meist' });
 });
 
+app.get('/kontakt', (req, res) => {
+    res.render('kontakt', { title: 'Kontakt' });
+});
+
+app.get('/loomad', (req, res) => {
+    readFile('./assets/loomad.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        const loomad = JSON.parse(data);
+
+        res.render('loomad', { title: 'Loomad', loomad: loomad });
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).render('404', { title: 'Leht ei leitud' });
+});
+
+app.listen(3000, () => {
+    console.log("Server jookseb portil 3000");
+});
